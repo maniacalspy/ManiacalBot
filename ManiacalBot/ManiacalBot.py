0aa1c6e1cc3bfbfa8a5ac079b906f71e97d45313
Nolan Roher <maniacalspy@gmail.com>
2021-03-21 16:39:47 -0400

    edits the bot to include mouse control inputs

--- ManiacalBot/ManiacalBot.py
+++ ManiacalBot/ManiacalBot.py
@@ -15,6 +15,11 @@ from dataclasses import dataclass
 
 import MBSQLModule as SQL
 
+#THIS IS FOR THE TTYD GAME INTEGRATION, IT ADDS THE FOLLOWING FOLDER TO THE PATH FOR IMPORTING FILES
+sys.path.insert(0, 'C:/Users/Nolan/Desktop/ManiacalBot/TwitchPlaysCode/TwitchChatIntegration')
+
+#TESTING NEW INPUT MODE
+import InputDataClasses
 
 ChannelInfo = namedtuple('ChannelInfo', ('broadcaster_language', 'user_name', 'displayed_name', 'game_id', 'id', 'is_live', 'tag_ids', 'thumbnail_url', 'title', 'started_at'))
 
@@ -108,7 +113,7 @@ import ctypes
 
 
 
-KeycodeGames = {"TTYD", "Pokemon"}
+KeycodeGames = {"TTYD", "Pokemon", "test", "Valheim"}
 
 #The input method used for the chat integration, such as using the KeyCodeInput class
 Input = None
@@ -215,7 +220,24 @@ class ManiacalBot(commands.Bot):
 
     async def HandleIntegration(self, ctx):
         message = ctx.content.strip(' ').lower()
-        if(self.GameTitle == "BeatBars"):
+
+        if self.GameTitle == "test":
+            if message == "look left":
+                await SendTestMouseInput(-480,0)
+            elif message == "look right":
+                await SendTestMouseInput(480,0)
+            elif len(message) == 1 or message in pyautogui.KEYBOARD_KEYS:
+                await SendTestKeyInput(message)
+            elif message == "space":
+                await SendKeyInputDuration(' ', 1)
+            elif message == "click":
+                await SendTestMouseClick()
+            elif message == "swap":
+                await SendMouseButtonSwap()
+            elif message == "info":
+                await PrintMouseInfo()
+
+        elif(self.GameTitle == "BeatBars"):
             if message == 'up' or message == 'north':
                 self.chat_data[ctx.author.name] = 'north'
             elif message == 'down' or message == 'south':
@@ -229,10 +251,43 @@ class ManiacalBot(commands.Bot):
             if message in GameInputData.keywords:
                 InputData = GameInputData.keywords[message]
                 await SendKeyInputDuration(Key = InputData.button, duration = InputData.duration)
-
+            else:
+                try:
+                    if GameInputData.GameInfo.MouseControlsEnabled:
+                        if message in GameInputData.MouseControlData.keywords:
+                            MouseData = GameInputData.MouseControlData.keywords[message]
+                            await self.HandleMouseInput(MouseData)
+                        else:
+                            try:
+                                if message in GameInputData.InputChords.keywords:
+                                    for item in GameInputData.InputChords.keywords[message]:
+                                        if item.__class__ is InputDataClasses.MouseData:
+                                            loop = asyncio.get_event_loop()
+                                            loop.create_task(self.HandleMouseInput(item))
+                                        elif item.__class__ is InputDataClasses.ButtonData:
+                                            loop = asyncio.get_event_loop()
+                                            loop.create_task(SendKeyInputDuration(Key = item.button, duration = item.duration))
+                                        else:
+                                            print(item.__class__)
+                            except Exception as e:
+                                print(e)
+                                pass
+                except Exception as e:
+                    print(e)
+                    pass
+        
         if len(self.chat_data) > 0:
             await self.Print_Chat_Data()
             return
+
+
+    async def HandleMouseInput(self, MouseData):
+            if MouseData.inputType == 'BUTTON':
+                await SendMouseClick(MouseData.button, MouseData.duration)
+            elif MouseData.inputType == 'MOVEMENT':
+                await SendMouseMoveInput(MouseData.xMovement, MouseData.yMovement,MouseData.duration)    
+            elif MouseData.inputType == 'SWAP':
+                await SendMouseButtonSwap(MouseData.duration)
 
     async def EnableChatReading(self, title):
         self.bIntegrationMode = ctypes.c_bool(True)
@@ -245,6 +300,7 @@ class ManiacalBot(commands.Bot):
         self.bIntegrationMode = ctypes.c_bool(False)
         self.GameTitle="NONE"
         global Input, GameInputData
+        Input.RevertMouseButton()
         Input = None
         GameInputData = None
 
@@ -581,7 +637,8 @@ async def ImportKeyCodeData(title):
             Input = importlib.import_module(modulename)
     else:
         Input = sys.modules[modulename]
-        
+
+
     GameData = title + "GameData"
     if GameData not in sys.modules:
         spec = importlib.util.find_spec(GameData)
@@ -606,6 +663,38 @@ async def SendKeyInputDuration(Key, duration):
     except e:
         print(e)
 
+
+async def SendTestKeyInput(Key):
+    pyautogui.press(Key)
+
+
+async def SendTestKeyInputDuration(Key, duration):
+    pyautogui.keyDown(Key)
+    await asyncio.sleep(duration)
+    pyautogui.keyUp(Key)
+
+
+@rate_limited(2,mode='kill')
+async def SendMouseMoveInput(X,Y,duration):
+    #pyautogui.moveTo(X,Y,.5,pyautogui.easeOutQuad)
+    #Input.set_pos(X,Y)
+    Input.move_mouse_relative(X,Y,duration)
+
+async def SendMouseClick(button, duration):
+    #pydirectinput.click()
+    #pydirectinput.press('esc')
+    #await SendKeyInput(0x100)
+    Input.click(button, duration)
+
+@rate_limited(10, mode='kill')
+async def SendMouseButtonSwap(duration):
+    Input.SwapMouseButton()
+    await asyncio.sleep(duration)
+    Input.RevertMouseButton()
+
+
+async def PrintMouseInfo():
+    Input.GetInfo()
 
 if __name__ == "__main__":
     bot = ManiacalBot()
